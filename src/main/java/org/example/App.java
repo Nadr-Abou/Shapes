974d8df0223b37e619aa0c23777feee995c7667b
Nadr-Abou <n.aboumoustafa@itsrizzoli.it>
2023-06-01 12:56:34 +0200

    Inizio progetto e creazione classi 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,5 +7,5 @@ public class App
 
     }
 }
-//si scriva una gerarchia di classi che rappresentano figure geometriche : "Shape", "Rectancle", "Circle", "Square", " e pure l'esagono". Si realizzi la gerarchia necessaria per il funzionamento, il dato comune è il centro, proprietà di classe "Cpoint" che ha X,Y si implementino i metodi, setter  e getter adeguati dopo aver creato i necessari attributi delle singole classi e si implementi il toString. si implementino i metodi area perimetro che derivano da un classe super ,perimetro e area nello shape è 0,
+//si scriva una gerafffrchia di classi che rappresentano figure geometriche : "Shape", "Rectancle", "Circle", "Square", " e pure l'esagono". Si realizzi la gerarchia necessaria per il funzionamento, il dato comune è il centro, proprietà di classe "Cpoint" che ha X,Y si implementino i metodi, setter  e getter adeguati dopo aver creato i necessari attributi delle singole classi e si implementi il toString. si implementino i metodi area perimetro che derivano da un classe super ,perimetro e area nello shape è 0,
 //scrivere test di un array di shape che verifichi la somma di tutte le aree e perimetro
